6adf13dcf09e0e44e598bb51f4066f5066eddbcc
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-07 10:59:16 +0530

    using appBar & style

--- 01.Basics/my_app/lib/main.dart
+++ 01.Basics/my_app/lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "MyApp",
-      home: Text("Hello, from Chandan Chaudhary"),
+      // home: Text("Hello, from Chandan Chaudhary"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Chandan Chaudhary",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blueAccent,
+              fontSize: 30,
+            ),
+          ),
+
+          centerTitle: true,
+        ),
+      ),
     );
   }
 }
