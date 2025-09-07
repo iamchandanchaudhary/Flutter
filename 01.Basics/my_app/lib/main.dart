dec3765677efc5e417afbc6999f604896826cb44
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-07 15:43:33 +0530

    Using Widget

--- 01.Basics/my_app/lib/main.dart
+++ 01.Basics/my_app/lib/main.dart
@@ -12,20 +12,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "MyApp",
       // home: Text("Hello, from Chandan Chaudhary"),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Chandan Chaudhary",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blueAccent,
-              fontSize: 30,
-            ),
-          ),
+      home: MyWidget(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
 
-          centerTitle: true,
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Chandan Chaudhary",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+            fontSize: 30,
+          ),
         ),
+
+        centerTitle: true,
       ),
+
+      body: Text("Hello Chandu Bhaiii"),
     );
   }
 }
